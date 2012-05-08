23b0f238027c853e833f61d91c697afeed840690
will.zou <will@poloniouslive.com>
2012-05-08 15:43:16 +1000

    test

--- regimo-core/src/main/java/au/com/regimo/core/repository/TextTemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package au.com.regimo.core.repository;
-
-import au.com.regimo.core.domain.TextTemplate;
-
-public interface TextTemplateRepository extends GenericRepository<TextTemplate, Long>{
-
-}
