025cd610a64dc1af9ceb9f9903f44c0d4bf4573e
will.zou <will@poloniouslive.com>
2012-05-14 14:54:07 +1000

    reset forgot password

--- regimo-web/src/main/java/au/com/regimo/web/UserController.java
+++ regimo-web/src/main/java/au/com/regimo/web/UserController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,13 +67,17 @@ public class UserController extends GenericEntityController<User> {
 
 	@Transactional
 	@RequestMapping(value = "/resetForgottenPwd", method = RequestMethod.POST)
-	public String resetForgottenPwd(@ModelAttribute UserListForm form) {
+	public String resetForgottenPwd(@ModelAttribute UserListForm form, ModelMap modelMap) {
 	
 		User user = userRepository.findByUsername(form.getUsername());
 		if(user != null){
 			entityService.resetPassword(user);
+			modelMap.addAttribute("resetPwdSuccessfully", "true");
 		}
-		return "redirect:view/"+user.getId();
+		else{
+			modelMap.addAttribute("resetPwdSubscribeFailed", "true");
+		}
+		return "signin";
 	}
 	
 	@Inject
