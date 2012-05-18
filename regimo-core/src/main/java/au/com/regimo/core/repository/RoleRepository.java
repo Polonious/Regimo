51a66cab6828e73a5eebdece37e026ef67e9c4aa
will.zou <will@poloniouslive.com>
2012-05-18 14:02:13 +1000

    admin menu

--- regimo-core/src/main/java/au/com/regimo/core/repository/RoleRepository.java
+++ regimo-core/src/main/java/au/com/regimo/core/repository/RoleRepository.java
@@ -1,9 +1,10 @@
 package au.com.regimo.core.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import au.com.regimo.core.domain.Role;
 
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Role findByName(String rolename);
 }
