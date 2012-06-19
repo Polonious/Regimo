1c05467230df8ad50cc622b2c80ec3fc7ac4daa0
Ted Liang <tedliang.email@gmail.com>
2012-06-19 11:29:21 +1000

    #4 admin menu refactor menu

--- regimo-web/src/main/java/au/com/regimo/web/WordpressController.java
+++ regimo-web/src/main/java/au/com/regimo/web/WordpressController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import au.com.regimo.core.repository.DashboardRepository;
 import au.com.regimo.server.wordpress.repository.WpPostRepository;
 import au.com.regimo.server.wordpress.repository.WpTermRepository;
 
@@ -17,7 +16,6 @@ public class WordpressController {
 
 	private WpTermRepository wpTermRepository;
 	private WpPostRepository wpPostRepository;
-	private DashboardRepository dashboardRepository;
 	
 	@RequestMapping(value = "")
 	public void browse(ModelMap map) {
@@ -26,7 +24,6 @@ public class WordpressController {
 	
 	@RequestMapping(value = "/category/{slug}")
 	public String getPostsByCategory(@PathVariable("slug") String slug, ModelMap map) {
-		map.addAttribute("menu", dashboardRepository.findByViewName("HomeMenu"));
 		map.addAttribute("posts", wpPostRepository.findBySlug(slug));
 		map.addAttribute("name",wpTermRepository.getNameBySlug(slug));
 		return "posts";
@@ -34,7 +31,6 @@ public class WordpressController {
 
 	@RequestMapping(value = "/post/{postName}")
 	public String getPost(@PathVariable("postName") String postName, ModelMap map) {
-		map.addAttribute("menu", dashboardRepository.findByViewName("HomeMenu"));
 		map.addAttribute("post", wpPostRepository.findByPostName(postName));
 		return "post";
 	}
@@ -47,11 +43,6 @@ public class WordpressController {
 	@Inject
 	public void setWpPostRepository(WpPostRepository wpPostRepository) {
 		this.wpPostRepository = wpPostRepository;
-	}
-
-	@Inject
-	public void setDashboardRepository(DashboardRepository dashboardRepository) {
-		this.dashboardRepository = dashboardRepository;
 	}
 
 }
--- regimo-web/src/main/java/au/com/regimo/web/spring/CustomTilesView.java
+++ regimo-web/src/main/java/au/com/regimo/web/spring/CustomTilesView.java
@@ -5,18 +5,37 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Hibernate;
 import org.springframework.web.context.support.ContextExposingHttpServletRequest;
 import org.springframework.web.servlet.view.tiles2.TilesView;
+
+import au.com.regimo.core.domain.Dashboard;
+import au.com.regimo.core.domain.UserDashlet;
+import au.com.regimo.core.repository.DashboardRepository;
 
 import com.google.common.collect.Sets;
 
 public class CustomTilesView extends TilesView {
 
+	private static String KEY_MENU = "menu";
+	
+	private Dashboard menu = null;
+	
 	@Override
 	protected void renderMergedOutputModel(
 			Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpServletRequest requestToExpose = new ContextExposingHttpServletRequest(
 				request, getWebApplicationContext(), Sets.newHashSet("referenceData"));
+		if(!model.containsKey(KEY_MENU)){
+			if(menu==null){
+				DashboardRepository r = getWebApplicationContext().getBean(DashboardRepository.class);
+				menu = r.findByViewName("HomeMenu");
+				for(UserDashlet ud: menu.getUserDashlets()){
+					Hibernate.initialize(ud.getDashlet());
+				}
+			}
+			model.put(KEY_MENU, menu);
+		}
 		super.renderMergedOutputModel(model, requestToExpose, response);
 	}
 	
